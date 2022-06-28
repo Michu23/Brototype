41bd4260ed6bc72bf94416d4f1db19db5d6d99b0
Michu <m4michu@gmail.com>
2022-06-29 00:39:44 +0530

    Minute bug fix

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -9,7 +9,7 @@ import FormData from "form-data";
 const AuthContext = createContext();
 export default AuthContext;
 
-export const BaseUrl = "http://127.0.0.1:8000/";
+export const BaseUrl = "https://api.brotocamp.space/";
 
 export const AuthProvider = ({ children }) => {
   const navigate = useNavigate();

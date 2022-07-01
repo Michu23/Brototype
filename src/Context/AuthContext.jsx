d5b469780fe73e24b13392ff228461f519f88283
sersha01 <skperson01@gmail.com>
2022-07-01 13:11:39 +0530

    department auto create sett

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -9,10 +9,10 @@ import FormData from "form-data";
 const AuthContext = createContext();
 export default AuthContext;
 
-export const BaseUrl = "https://api.brotocamp.space/";
-export const BaseLink = "https://brotocamp.space/";
-// export const BaseUrl = "http://127.0.0.1:8000/";
-// export const BaseLink = "http://localhost:3000/";
+// export const BaseUrl = "https://api.brotocamp.space/";
+// export const BaseLink = "https://brotocamp.space/";
+export const BaseUrl = "http://127.0.0.1:8000/";
+export const BaseLink = "http://localhost:3000/";
 
 export const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
